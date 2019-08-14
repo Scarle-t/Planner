2cd755b60f316560ca11c22fd8663be8a73d02c0
Scarlet <info_ng@me.com>
2019-08-14 23:23:30 +0800

    Initial Commit

---
+++ Planner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Planner
+//
+//  Created by Scarlet on A2019/A/14.
+//  Copyright © 2019 Scarlet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
