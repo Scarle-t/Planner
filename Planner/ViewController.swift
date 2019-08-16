6df1e849aa57d836081a5318e643a59e35771fc3
Scarlet <info_ng@me.com>
2019-08-17 04:25:05 +0800

    Working animation

--- Planner/ViewController.swift
+++ Planner/ViewController.swift
@@ -16,6 +16,22 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     @IBOutlet weak var heading: UILabel!
     
     var cellTransform = CGAffineTransform()
+    var currentIndex = IndexPath()
+    var currentCell = UICollectionViewCell()
+    var currentFrame = CGRect()
+    
+    @objc func closeCell(_ sender: UIButton){
+        UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseIn, animations: {
+            sender.alpha = 0
+            self.currentCell.frame = self.currentFrame
+        }, completion: { _ in
+            UIView.performWithoutAnimation {
+                self.plans.reloadItems(at: [self.currentIndex])
+            }
+        })
+        plans.isScrollEnabled = true
+        plans.allowsSelection = true
+    }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 5
@@ -32,7 +48,39 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         cell.layer.shadowColor = UIColor.lightGray.cgColor
         cell.layer.shadowOpacity = 0.7
         
+        let closeBtn = UIButton(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        closeBtn.tag = 2
+        closeBtn.addTarget(self, action: #selector(closeCell(_:)), for: .touchUpInside)
+        
+        closeBtn.frame.origin.x = cell.frame.width - 60
+        closeBtn.frame.origin.y = 10
+        if #available(iOS 13.0, *) {
+            closeBtn.setImage(UIImage(systemName: "chevron.down.circle.fill"), for: .normal)
+        } else {
+            // Fallback on earlier versions
+            closeBtn.setTitle("Close", for: .normal)
+        }
+        closeBtn.alpha = 0
+        
+        cell.addSubview(closeBtn)
+        
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)!
+        currentCell = cell
+        currentIndex = indexPath
+        currentFrame = cell.frame
+        cell.superview?.bringSubviewToFront(cell)
+        
+        UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseIn, animations: {
+            cell.frame = collectionView.bounds
+            cell.viewWithTag(2)?.alpha = 1
+        }, completion: nil)
+        
+        collectionView.isScrollEnabled = false
+        collectionView.allowsSelection = false
     }
     
     func collectionView(_ collectionView: UICollectionView, didHighlightItemAt indexPath: IndexPath) {
@@ -61,16 +109,16 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         plans.delegate = self
         plans.dataSource = self
         
-        
-        
         plans.collectionViewLayout = CardsCollectionViewLayout()
         
         let layout = plans.collectionViewLayout as! CardsCollectionViewLayout
-        layout.itemSize = .init(width: plans.frame.width - 100, height: plans.frame.height - 250)
+        layout.itemSize = .init(width: self.view.bounds.width * 0.8, height: self.view.bounds.height * 0.7)
         layout.spacing = 30
         
         plans.isPagingEnabled = true
         plans.showsHorizontalScrollIndicator = false
+        
+        
         
     }
     
@@ -87,10 +135,8 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
                 self.plans.alpha = 1
             }, completion: nil)
         })
-        
         plans.reloadData()
     }
-
 
 }
 
